165f6eefc2465d19beaf963ee4d6d9071c120070
Tingting Wei <twei3@academic.rrc.ca>
2024-03-18 23:34:34 -0500

    add category route for the view request

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
+  has_many :products
+  
   def self.ransackable_attributes(auth_object = nil)
     ["category_name", "created_at", "id", "updated_at"]
   end
--- config/routes.rb
+++ config/routes.rb
@@ -19,4 +19,6 @@ Rails.application.routes.draw do
   end
 
   root 'products#index'
+
+  resources :categories
 end
