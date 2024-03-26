28266d2c9703cc8611cfd16644154c6e0d714b38
Tingting Wei <twei3@academic.rrc.ca>
2024-03-25 20:09:10 -0500

    generate devise user and migrate the database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   validates :email, uniqueness: true
 
   def self.ransackable_associations(auth_object = nil)
