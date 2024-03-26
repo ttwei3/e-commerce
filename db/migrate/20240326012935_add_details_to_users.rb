d6c547f06f5c74dc6ed112ae00906ff96ded48ba
Tingting Wei <twei3@academic.rrc.ca>
2024-03-25 20:30:10 -0500

    generate province model and add details to user model

---
+++ db/migrate/20240326012935_add_details_to_users.rb
@@ -0,0 +1,8 @@
+class AddDetailsToUsers < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :address, :string
+    add_column :users, :city, :string
+    add_column :users, :postal_code, :string
+    add_reference :users, :province, null: false, foreign_key: true
+  end
+end
