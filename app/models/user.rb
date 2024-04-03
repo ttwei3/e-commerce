923f573c9a17482a9a0324a4909379523b5cd56d
Tingting Wei <twei3@academic.rrc.ca>
2024-04-03 16:56:52 -0500

    generate Orders controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,6 @@ class User < ApplicationRecord
   end
 
   def self.ransackable_attributes(auth_object = nil)
-    %w[first_name last_name email address phone_number role]
+    %w[first_name last_name email address city postal_code phone_number role]
   end
 end
